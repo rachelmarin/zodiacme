5c5d578ff739a320d5585e220abf46409bdca9da
Rachel Marin <68628609+rachelmarin@users.noreply.github.com>
2022-06-26 12:22:53 -0400

    css

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,16 @@ const Navbar = () => {
       <nav className="navbar">
         <h1>Zodiac Me</h1>
         <div className="links">
-            <a href="/">Home </a>
-            <a href="/create"> New</a>
+            <a href="/" style={{
+                color: "white",
+                backgroundColor: '#4435c4',
+                borderRadius: '10px'
+            }}>Home </a>
+            <a href="/create" style={{
+                color: "white",
+                backgroundColor: '#4435c4',
+                borderRadius: '10px'
+            }}> New</a>
 
         </div>
       </nav>  
