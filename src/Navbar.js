ac7e6a15db007c7bcf937ec0d5e83c7e9d54c04f
Rachel Marin <68628609+rachelmarin@users.noreply.github.com>
2022-06-26 09:26:35 -0400

    nav

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+      <nav className="navbar">
+        <h1>Zodiac Me</h1>
+        <div className="links">
+            <a href="/">Home </a>
+            <a href="/create"> New</a>
+
+        </div>
+      </nav>  
+     );
+}
+ 
+export default Navbar;
