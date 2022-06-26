7602458d058327ca732c164e9177761e084c4de8
Rachel Marin <68628609+rachelmarin@users.noreply.github.com>
2022-06-26 08:59:57 -0400

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 function App() {
+  const title = 'Zodiac Me';
+  
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
       </div>
     </div>
   );
