0e815d08a0310a0ac7aa179536cf46b14fbe4d33
Rachel Marin <68628609+rachelmarin@users.noreply.github.com>
2022-06-28 07:58:12 -0400

    Home button

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    let name = 'mario'
+
+  const handleClick = () => {
+    console.log('Hello, Beautiful!');
+  }  
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 }
