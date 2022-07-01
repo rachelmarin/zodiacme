47943ea28745e7a1c3ce68b8d231b91318dc5277
Rachel Marin <68628609+rachelmarin@users.noreply.github.com>
2022-06-30 20:00:57 -0400

    age

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
     const Home = () => {
     //let name = 'mario'
     const [name, setName] = useState('mario');
+    const [age, setAge] = useState('25');
 
   const handleClick = () => {
     setName('luigi');
@@ -11,7 +12,7 @@ import { useState } from 'react';
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is { age } years old</p>
             <button onClick={handleClick}>Click Me</button>
         </div>
     );
